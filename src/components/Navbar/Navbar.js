7c7cc215974946e62747e4c1547b8c5cde97ca5e
lahirutech <rmllbrathnayaka@gmail.com>
2022-09-05 12:51:10 +0530

    Adding pages

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function Navbar() {
+    const drawerWidth = 200
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                    backgroundColor: "#101F33",
+                    color: "#FFFFFF"
+
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Toolbar />
+            <Divider />
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon sx={{ color: "#FFFFFF" }}>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon sx={{ color: "#FFFFFF" }}>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    )
+}
---
+++ src/components/pages/Functions/Functions.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Functions() {
+  return (
+    <div>Functions</div>
+  )
+}
