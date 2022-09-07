1b1f41f361b2d70f909501c1dec87589af2f077d
lahirutech <rmllbrathnayaka@gmail.com>
2022-09-07 21:45:33 +0530

    adding dynamic title

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import { Outlet } from 'react-router-dom';
 import { Grid, ThemeProvider } from '@mui/material';
 import { dashboardTheme } from './dashboardTheme';
 import Header from './components/Header/Header';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 
 function App() {
+  const [title, setTitle] = useState(null)
+  const location = useLocation();
+
+  useEffect(() => {
+    const parsedTitle = location.pathname.replace(/\W/g, ' ');
+    setTitle(parsedTitle);
+  }, [location])
+
   return (
     <Grid container>
       <Navbar />
-      <Header/>
+      <Header title={title} />
       <Outlet />
     </Grid>
 
