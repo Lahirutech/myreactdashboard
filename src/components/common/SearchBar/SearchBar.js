f61560aabb9a4d33a719922f3d1b38acaaa0961d
lahirutech <rmllbrathnayaka@gmail.com>
2022-09-09 15:53:36 +0530

    modify the search

---
+++ src/components/common/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import { Box, Input } from '@mui/material'
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function SearchBar({placeholder, onChange, searchBarWidth}) {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <SearchIcon sx={{ marginRight: '10px' }} />
+            <Input disableUnderline placeholder={placeholder} sx={{ width: {searchBarWidth},onChange:{onChange}, color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.1rem' }}></Input> 
+        </Box>
+    )
+}
