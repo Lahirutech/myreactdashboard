1046731d023ae7ebaea4140196556f20d05d9b63
lahirutech <rmllbrathnayaka@gmail.com>
2022-09-10 22:21:08 +0530

    adding the search

--- src/components/pages/Authentication/Authentication.js
+++ src/components/pages/Authentication/Authentication.js
@@ -8,62 +8,50 @@ import GridWrapper from '../../common/GridWrapper/GridWrapper';
 import SearchBar from '../../common/SearchBar/SearchBar';
 import BasicCard from '../../common/BasicCard/BasicCard';
 import NewUserModal from '../../Modals/NewUserModal/NewUserModal';
+import { cardHeaderStyles } from './styles';
 
 export default function Authentication() {
 
   const [open, setOpen] = useState(false);
   const [users, setUsers] = useState([]);
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
-  const cardStyles = {
-    cardHeader: {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      paddingLeft: '20px',
-      paddingRight: '20px',
-      height: '65px',
-      backgroundColor: '#f5f5f5',
-      borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-
-
-    },
-    addUserButton: {
-      fontSize: '1.05rem',
-    },
-  }
+  const [searchResults, setSearchResults] = useState(users);
 
   const getHeader = () => {
 
-    const handleChange = (value) => {
-      console.log(value)
-    };
+    const handleSearch = (value) => {
+      filterData(value);
+  };
 
     const addUser = () => {
       setOpen(true);
-  };
+    };
+
+    const filterData = (value) => {
+      const lowercasedValue = value.toLowerCase().trim()
+      if (lowercasedValue === '') setUsers(searchResults)
+      else {
+        const filteredData = searchResults.filter((item) => {
+          return Object.keys(item).some((key) =>
+            item[key].toString().toLowerCase().includes(lowercasedValue)
+          )
+        })
+        setUsers(filteredData)
+      }
+
+    }
+
+
     return (
-      <Box sx={cardStyles.cardHeader}>
+      <Box sx={cardHeaderStyles.wrapper}>
         <SearchBar
           placeholder="placeholder"
-          onChange={(e) => handleChange(e)}
+          onChange={(e) => handleSearch(e.target.value)}
           searchBarWidth="720px" />
         <Box>
           <CommonButton
             onClick={addUser}
             size="large"
-            variant="contained" sx={cardStyles.addUserButton}>
+            variant="contained" sx={cardHeaderStyles.addUserButton}>
             Add user
           </CommonButton>
           <IconButton>
@@ -78,17 +66,30 @@ export default function Authentication() {
   }
 
   const getContent = () => (
-    <Typography
-      align="center"
-      sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem' }}>
-      No users for this project yet
-    </Typography>
+    <>
+      {
+        users.length ?
+          users.map((user) => (
+            <Box sx={{ marginBottom: '20px' }}>
+              <Typography>User ID: {user.userId}</Typography>
+              <Typography>Email: {user.email}</Typography>
+              <Typography>Phone Number: {user.phoneNumber}</Typography>
+            </Box>
+          )) :
+
+          < Typography
+            align="center"
+            sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem' }}>
+            No users for this project yet
+          </Typography>
+      }
+    </>
   )
 
   const addNewUser = (data) => {
     users.push({ ...data });
     setOpen(false);
-};
+  };
 
 
   return (
@@ -97,7 +98,7 @@ export default function Authentication() {
       <BasicCard
         header={getHeader()}
         content={getContent()} />
-      
+
       <NewUserModal open={open} onClose={() => setOpen(false)} addNewUser={addNewUser} />
 
     </GridWrapper>
